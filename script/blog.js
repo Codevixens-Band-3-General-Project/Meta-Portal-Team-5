61047a89ee6ab417d9451d111a5bf023e36429f3
KrownWealth <abdulramonadeola36@gmail.com>
2023-07-05 18:42:25 +0100

    Blog page content and JS functionality added, colors, and font needs adjustment

---
+++ script/blog.js
@@ -0,0 +1,115 @@
+
+let blogData =[
+  {
+    id:0,
+    title:"Reasons Why People Use Discord",
+    img:"./Assets/blog-img-1.jpg",
+    btn:"Read More"
+
+  },
+  {
+    id:1,
+    title:"Reasons Why People Use Discord",
+    img:"./Assets/blog-img-2.jpg",
+    btn:"Read More"
+
+  },
+  {
+    id:2,
+    title:"Reasons Why People Use Discord",
+    img:"./Assets/blog-img-3.jpg",
+    btn:"Read More"
+
+  },
+  {
+    id:3,
+    title:"Reasons Why People Use Discord",
+    img:"./Assets/blog-img-4.jpg",
+    btn:"Read More"
+
+  },
+  {
+    id:4,
+    title:"Reasons Why People Use Discord",
+    img:"./Assets/blog-img-1.jpg",
+    btn:"Read More"
+
+  },
+  {
+    id:5,
+    title:"Reasons Why People Use Discord",
+    img:"./Assets/blog-img-1.jpg",
+    btn:"Read More"
+
+  },
+  {
+    id:6,
+    title:"Reasons Why People Use Discord",
+    img:"./Assets/blog-img-1.jpg",
+    btn:"Read More"
+
+  },
+  {
+    id:7,
+    title:"Reasons Why People Use Discord",
+    img:"./Assets/blog-img-1.jpg",
+    btn:"Read More"
+
+  },
+  {
+    id:8,
+    title:"Reasons Why People Use Discord",
+    img:"./Assets/blog-img-1.jpg",
+    btn:"Read More"
+
+  },
+  {
+    id:9,
+    title:"Reasons Why People Use Discord",
+    img:"./Assets/blog-img-1.jpg",
+    btn:"Read More"
+
+  },
+  {
+    id:10,
+    title:"Reasons Why People Use Discord",
+    img:"./Assets/blog-img-1.jpg",
+    btn:"Read More"
+
+  },
+  {
+    id:11,
+    title:"Reasons Why People Use Discord",
+    img:"./Assets/blog-img-1.jpg",
+    btn:"Read More"
+
+  },
+  {
+    id:12,
+    title:"Reasons Why People Use Discord",
+    img:"./Assets/blog-img-1.jpg",
+    btn:"Read More"
+
+  },
+  {
+    id:13,
+    title:"Reasons Why People Use Discord",
+    img:"./Assets/blog-img-1.jpg",
+    btn:"Read More"
+
+  },
+  {
+    id:14,
+    title:"Reasons Why People Use Discord",
+    img:"./Assets/blog-img-1.jpg",
+    btn:"Read More"
+
+  },
+  {
+    id:15,
+    title:"Reasons Why People Use Discord",
+    img:"./Assets/blog-img-1.jpg",
+    btn:"Read More"
+
+  },
+]
